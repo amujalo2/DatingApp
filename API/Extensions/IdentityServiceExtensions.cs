615abb559505de9fb53e16bf036204c833afe625
amujalo2 <amer.mujalo@htecgroup.com>
2025-04-27 14:27:58 +0200

    End of section 4

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection addIdentityServices(this IServiceCollection services, IConfiguration config)
+    {
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+                {
+                    var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey is not set in the configuration.");
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey))
+                    };
+                });
+        return services;
+    }
+}
