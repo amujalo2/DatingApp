ac33f455bdaf0f5bdc705a5b70fa8ada42cbe452
amujalo2 <amer.mujalo@htecgroup.com>
2025-06-12 10:26:46 +0200

    added  loadingSubject with BehaviorSubject for loading

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,15 +5,18 @@ import { AccountService } from './_services/account.service';
 import { NgxSpinnerComponent } from 'ngx-spinner';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthStoreService } from './_services/auth-store.service';
+import { BusyService } from './_services/busy.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, NgxSpinnerComponent, FontAwesomeModule],
+  imports: [RouterOutlet, NavComponent, NgxSpinnerComponent, FontAwesomeModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   private authStoreService = inject(AuthStoreService);
+  busyService = inject(BusyService);
   ngOnInit(): void {
     this.setCurrentUser();
   }
